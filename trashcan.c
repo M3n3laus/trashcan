fa52bcf08cf2dde6d1b4d0a0647d45b5ca59a8db
Samuel Maddox <smaddox@e1z3r3p4.42.us.org>
2019-07-04 16:26:39 -0700

    no macros but passes basic tests

--- trashcan.c
+++ trashcan.c
@@ -1,13 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   trashcan.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: smaddox <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/07/04 16:24:56 by smaddox           #+#    #+#             */
+/*   Updated: 2019/07/04 16:25:01 by smaddox          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "trashcan.h"
-#include <stdarg.h>
-#include <stdlib.h>
 
 void new_trash_bag(t_rash_can *my_trashcan)
 {
 	my_trashcan->top = -1;
 }
 
-void throw_away(t_rash_can *my_trashcan, e_my_type tp, ...)
+void mom(t_rash_can *my_trashcan, e_my_type tp, ...)
 {
 	va_list list;
 	if (!trash_overflow(my_trashcan))
@@ -16,7 +26,7 @@ void throw_away(t_rash_can *my_trashcan, e_my_type tp, ...)
 		(my_trashcan->top)++;
 		my_trashcan->can[my_trashcan->top].type = tp;
 		
-		if (my_trashcan->can[my_trashcan->top].type == T_CHAR)	
+		if (my_trashcan->can[my_trashcan->top].type == T_CHAR)
 			my_trashcan->can[my_trashcan->top].u_piece.cp = va_arg(list, char*);
 		else if (my_trashcan->can[my_trashcan->top].type == T_INT)
 			my_trashcan->can[my_trashcan->top].u_piece.ip = va_arg(list, int*);
@@ -46,7 +56,7 @@ void garbage_day(t_rash_can *my_trashcan)
 {
 	while(!trashcan_empty(my_trashcan))
 	{
-		if (my_trashcan->can[my_trashcan->top].type == T_CHAR)	
+		if (my_trashcan->can[my_trashcan->top].type == T_CHAR)
 			free(my_trashcan->can[my_trashcan->top].u_piece.cp);
 		else if (my_trashcan->can[my_trashcan->top].type == T_INT)
 			free(my_trashcan->can[my_trashcan->top].u_piece.ip);
--- trashcan.h
+++ trashcan.h
@@ -6,9 +6,12 @@
 /*   By: smaddox <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/04 13:27:24 by smaddox           #+#    #+#             */
-/*   Updated: 2019/07/04 14:50:54 by smaddox          ###   ########.fr       */
+/*   Updated: 2019/07/04 16:25:06 by smaddox          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <stdarg.h>
+#include <stdlib.h>
 
 #define DEFAULT_SIZE 50
 typedef enum
@@ -36,7 +39,7 @@ typedef struct		s_trashcan
 }					t_rash_can;
 
 void				new_trash_bag(t_rash_can *my_trashcan);
-void				throw_away(t_rash_can *my_trashcan, e_my_type tp, ...);
+void				mom(t_rash_can *my_trashcan, e_my_type tp, ...);
 int					trash_overflow(t_rash_can *my_trashcan);
 int					trashcan_empty(t_rash_can *my_trashcan);
 void				garbage_day(t_rash_can *my_trashcan);
