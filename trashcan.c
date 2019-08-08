3062be35aeb6e27221521bbe575342d5188b37f2
Samuel Maddox <smaddox@e1z3r3p4.42.us.org>
2019-08-08 12:29:43 -0700

    normed

--- trashcan.c
+++ trashcan.c
@@ -6,7 +6,7 @@
 /*   By: smaddox <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/04 16:24:56 by smaddox           #+#    #+#             */
-/*   Updated: 2019/07/04 17:08:29 by smaddox          ###   ########.fr       */
+/*   Updated: 2019/08/08 12:25:24 by smaddox          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 
 t_rash_can g_can = {.top = -1};
 
-void mom(void *pointer)
+void	mom(void *pointer)
 {
 	if (!trash_overflow())
 	{
@@ -24,23 +24,23 @@ void mom(void *pointer)
 	}
 }
 
-int trash_overflow(void)
+int		trash_overflow(void)
 {
-	if(g_can.top == DEFAULT_SIZE - 1)
-		return(1);
-	return(0);
+	if (g_can.top == DEFAULT_SIZE - 1)
+		return (1);
+	return (0);
 }
 
-int trashcan_empty(void)
+int		trashcan_empty(void)
 {
 	if (g_can.top == -1)
-		return(1);
-	return(0);
+		return (1);
+	return (0);
 }
 
-void garbage_day(void)
+void	garbage_day(void)
 {
-	while(!trashcan_empty())
+	while (!trashcan_empty())
 	{
 		free(g_can.can[g_can.top]);
 		g_can.top--;
--- trashcan.h
+++ trashcan.h
@@ -6,28 +6,28 @@
 /*   By: smaddox <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/04 13:27:24 by smaddox           #+#    #+#             */
-/*   Updated: 2019/07/04 17:08:35 by smaddox          ###   ########.fr       */
+/*   Updated: 2019/08/08 12:29:17 by smaddox          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef TRASH_CAN_H
-# define TRASH_CAN_H
+#ifndef TRASHCAN_H
+# define TRASHCAN_H
 
-#include <stdarg.h>
-#include <stdlib.h>
+# include <stdarg.h>
+# include <stdlib.h>
 
-#define DEFAULT_SIZE 200
+# define DEFAULT_SIZE 200
 
-typedef struct				s_trashcan
+typedef struct		s_trashcan
 {
-	void				*can[DEFAULT_SIZE];
+	void			*can[DEFAULT_SIZE];
 	int				top;
 }					t_rash_can;
 
-void					mom(void *pointer);
+void				mom(void *pointer);
 int					trash_overflow(void);
 int					trashcan_empty(void);
-void					garbage_day(void);
+void				garbage_day(void);
 
 extern t_rash_can g_can;
 
